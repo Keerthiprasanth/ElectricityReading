0b68501bb7a000316983be4cfb6d2fc343d94595
Keerthiprasanth <rkpkeerthi22@gmail.com>
2022-12-22 03:46:54 +0000

    Updated toasts with print statements

--- app/src/main/java/com/example/myloginapplication/MainActivity.java
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -8,16 +8,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
+import com.example.myloginapplication.Model.Member;
 
 public class MainActivity extends AppCompatActivity {
-
+    Member member;
+    String name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//        member.setUsername(String.valueOf(R.id.username));
 
         TextView username = findViewById(R.id.username);
         TextView password = findViewById(R.id.password);
@@ -29,8 +30,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                     Toast.makeText(MainActivity.this,"Welcome to Break Up!",Toast.LENGTH_LONG).show();
-                }else{
+                }else if(!username.getText().toString().equals("admin"))
+                {
+                    Toast.makeText(MainActivity.this,"Enter valid username",Toast.LENGTH_LONG).show();
+                }
+                else{
                     Toast.makeText(MainActivity.this,"Enter valid credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        signinbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                name = username.getText().toString();
+                if(username.getText().toString().equals("ram") && password.getText().toString().equals("ram")){
+                    Toast.makeText(MainActivity.this,"Welcome "+name ,Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this,"Bye "+username.getText().toString(),Toast.LENGTH_LONG).show();
                 }
             }
         });
