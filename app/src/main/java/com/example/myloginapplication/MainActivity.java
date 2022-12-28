08138c9ba4b57c45269682ae0cfaf04fd454d6c2
Keerthiprasanth <rkpkeerthi22@gmail.com>
2022-12-28 23:57:31 +0000

    Build gradle updated

--- app/src/main/java/com/example/myloginapplication/MainActivity.java
+++ app/src/main/java/com/example/myloginapplication/MainActivity.java
@@ -4,15 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.FutureTask;
+
+import io.realm.Realm;
+import io.realm.mongodb.App;
+import io.realm.mongodb.AppConfiguration;
+import io.realm.mongodb.Credentials;
+import io.realm.mongodb.User;
+import io.realm.mongodb.sync.SyncConfiguration;
+
 public class MainActivity extends AppCompatActivity {
+    Realm uiThreadRealm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm.init(this);   //Mongodb realm initialisation
+        String appID = "application-0-joalz";
+        App app = new App(new AppConfiguration.Builder(appID)
+                .build());
+
+        Credentials credentials = Credentials.anonymous();
+        app.loginAsync(credentials, result -> {
+            if (result.isSuccess()) {
+                Log.v("QUICKSTART", "Successfully authenticated anonymously.");
+                User user = app.currentUser();
+                String partitionValue = "My Project";
+                SyncConfiguration config = new SyncConfiguration.Builder(
+                        user,
+                        partitionValue)
+                        .build();
+                uiThreadRealm = Realm.getInstance(config);
+                addChangeListenerToRealm(uiThreadRealm);
+                FutureTask<String> task = new FutureTask(new BackgroundQuickStart(app.currentUser()), "test");
+                ExecutorService executorService = Executors.newFixedThreadPool(2);
+                executorService.execute(task);
+            } else {
+                Log.e("QUICKSTART", "Failed to log in. Error: " + result.getError());
+            }
+        });
 
         Button signupbtn = findViewById(R.id.signupbtn);
         Button signinbtn = findViewById(R.id.signinbtn);
